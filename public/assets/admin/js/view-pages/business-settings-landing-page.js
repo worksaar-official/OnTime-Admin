97994e0f896d8a43ee95bbca023219c96d6b914a
WorkSaar Expert <paswan.narayan@gmail.com>
2026-04-23 12:07:48 +0530

    empty files

--- public/assets/admin/js/view-pages/business-settings-landing-page.js
+++
@@ -1,8 +0,0 @@
-
-
-"use strict";
-
-$('input[name="landing_integration_via"]').on('change', function() {
-    $(`.__input-tab`).removeClass('active')
-    $(`#${this.value}`).addClass('active')
-})
