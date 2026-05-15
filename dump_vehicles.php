e3d786482d5ea52844cf7d5fe61e9d3717c7d0a7
devsumitkarn <devsumitkarn@gmail.com>
2026-05-15 16:30:44 +0530

    deleted check function

--- dump_vehicles.php
+++ dump_vehicles.php
@@ -4,7 +4,7 @@ require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-$vehicles = DB::table('d_m_vehicles')->get();
+$vehicles = DB::table('delivery_man_vehicles')->get();
 foreach ($vehicles as $v) {
     echo "ID: {$v->id}, Name: {$v->type}, Extra Charge: {$v->extra_charges}\n";
 }
